2c685f0050c12a76a258bacb2860c4e5f7ec54f0
Kunal Chaudhary <98645631+KunalCh23@users.noreply.github.com>
2023-06-13 11:17:33 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 docker rm my-js-app-container || true
 
 # Start a new container
-docker run -d --name my-js-app-container -p 80:80 public.ecr.aws/j9v3f8u1/angular-app/my-js-app:latest
+docker run -d --name my-js-app-container -p 80:80 public.ecr.aws/j9v3f8u1/angular-app:latest
